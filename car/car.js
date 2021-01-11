6b4e8a415339ad68eb049630bf07958bf55e0fa4
JavaScript 2020/21 <javascript202021@Admins-iMac-84.local>
2021-01-11 20:57:08 +0200

    curs 3

--- car/car.js
+++ car/car.js
@@ -5,8 +5,28 @@ let dacia = {
     year: 1978,
     mileage: 525737,
     maxSpeed: 180,
+    averageConsumption: 11.5,
+    batteryCapacity:150,
+    batteryCharge: 150,
     paint: function(color) {
         this.color = color
+    },
+    move: function(distance) {
+        consumption = this.averageConsumption * distance / 100
+        if (consumption > this.batteryCharge) {
+            this.batteryCharge = 0
+            this.mileage += this.distanceToEmpty()
+        } else {
+            this.batteryCharge -= consumption
+            this.mileage += distance 
+        }
+        
+    },
+    recharge: function() {
+        this.batteryCharge = this.batteryCapacity
+    },
+    distanceToEmpty: function() {
+        return 100 * this.batteryCharge * this.averageConsumption
     }
 }
 let ferrari = {
@@ -21,7 +41,11 @@ let ferrari = {
     }
 }
 dacia.paint("galben")
+dacia.move(100)
+
+console.log (Math.round(dacia.distanceToEmpty()))
 console.log(dacia)
+console.log (dacia.distanceToEmpty())
 console.log(ferrari)
 
 
---
+++ lenght/lenght.js
@@ -0,0 +1,7 @@
+document.getElementById("text")
+.addEventListener("change", function(){
+    input = document.getElementById("text")
+    text = input.value
+    document.getElementById("length").innerText = text.length
+    
+})
